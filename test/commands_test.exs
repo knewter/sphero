cfa9c5ad1fa0e54db8a54de08e8c4ce4b98a9c60
Josh Adams <josh@isotope11.com>
2013-12-23 11:13:57 -0600

    Finish remaining commands

--- test/commands_test.exs
+++ test/commands_test.exs
@@ -12,9 +12,40 @@ defmodule Sphero.Command.Heading do
   end
 end
 
+defmodule Sphero.Command.SetBackLEDOutput do
+  def new([seq: seq, data: data]) do
+    Sphero.Command.new(seq: seq, data: data, cid: <<21>>)
+  end
+end
+
+defmodule Sphero.Command.SetRotationRate do
+  def new([seq: seq, data: data]) do
+    Sphero.Command.new(seq: seq, data: data, cid: <<3>>)
+  end
+end
+
+defmodule Sphero.Command.SetRGB do
+  def new([seq: seq, data: data]) do
+    Sphero.Command.new(seq: seq, data: data, cid: <<20>>)
+  end
+end
+
+defmodule Sphero.Command.GetRGB do
+  def new([seq: seq, data: data]) do
+    Sphero.Command.new(seq: seq, data: data, cid: <<22>>)
+  end
+end
+
 defmodule Sphero.Command.SetDataStreaming do
   def new([seq: seq, n: n, m: m, mask: mask, pcnt: pcnt, mask2: mask2]) do
     data = <<n :: [unsigned, size(16)], m :: [unsigned, size(16)], mask :: [unsigned, size(32)], pcnt, mask2 :: [unsigned, size(32)]>>
+    Sphero.Command.new(seq: seq, data: data, cid: <<12>>)
+  end
+end
+
+defmodule Sphero.Command.ConfigureCollisionDetection do
+  def new([seq: seq, meth: meth, x_t: x_t, y_t: y_t, x_spd: x_spd, y_spd: y_spd, dead: dead]) do
+    data = <<meth, x_t, y_t, x_spd, y_spd, dead>>
     Sphero.Command.new(seq: seq, data: data, cid: <<12>>)
   end
 end
@@ -34,9 +65,45 @@ defmodule CommandsTest do
     assert expected_bytes == Sphero.Request.to_string(request)
   end
 
+  test "Heading" do
+    request = Sphero.Command.Heading.new seq: 0, heading: 2
+    expected_bytes = <<255, 255, 2, 1, 0, 3, 0, 2, 247>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
+  test "SetBackLEDOutput" do
+    request = Sphero.Command.SetBackLEDOutput.new seq: 0, data: <<1,2,3>>
+    expected_bytes = <<255, 255, 2, 21, 0, 4, 1, 2, 3, 222>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
+  test "SetRotationRate" do
+    request = Sphero.Command.SetRotationRate.new seq: 0, data: <<1,2,3>>
+    expected_bytes = <<255, 255, 2, 3, 0, 4, 1, 2, 3, 240>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
+  test "SetRGB" do
+    request = Sphero.Command.SetRGB.new seq: 0, data: <<1,2,3>>
+    expected_bytes = <<255, 255, 2, 20, 0, 4, 1, 2, 3, 223>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
+  test "GetRGB" do
+    request = Sphero.Command.GetRGB.new seq: 0, data: <<1,2,3>>
+    expected_bytes = <<255, 255, 2, 22, 0, 4, 1, 2, 3, 221>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
   test "SetDataStreaming" do
     request = Sphero.Command.SetDataStreaming.new seq: 0, n: 1, m: 2, mask: 3, pcnt: 4, mask2: 5
     expected_bytes = <<255, 255, 2, 12, 0, 14, 0, 1, 0, 2, 0, 0, 0, 3, 4, 0, 0, 0, 5, 212>>
+    assert expected_bytes == Sphero.Request.to_string(request)
+  end
+
+  test "ConfigureCollisionDetection" do
+    request = Sphero.Command.ConfigureCollisionDetection.new seq: 0, meth: 1, x_t: 2, y_t: 3, x_spd: 4, y_spd: 5, dead: 6
+    expected_bytes = <<255, 255, 2, 12, 0, 7, 1, 2, 3, 4, 5, 6, 213>>
     assert expected_bytes == Sphero.Request.to_string(request)
   end
 end
