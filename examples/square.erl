ae0aa808bf5e9e566b45c54d345ce1e20482a504
Robby Clements <rclements@isotope11.com>
2014-02-24 23:35:38 -0600

    working on erlang version of sphero square

---
+++ examples/square.erl
@@ -0,0 +1,17 @@
+-module(square).
+-export([roll/1]).
+
+roll(Device) ->
+  {ok, S} = 'Elixir.Sphero.Client':start(Device),
+  timer:sleep(3000),
+  roll_and_sleep(S, 0),
+  roll_and_sleep(S, 90),
+  roll_and_sleep(S, 180),
+  roll_and_sleep(S, 270),
+  'Elixir.Sphero:Client':stop(s),
+  exit(S, kill).
+
+roll_and_sleep(S, Angle) ->
+  'Elixir.Sphero.Client':roll(S, 20, Angle),
+  timer:sleep(3000).
+
